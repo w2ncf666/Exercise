dc18112f1d09460e313104c3aacf45637e520358
w2ncf666 <1390845603@qq.com>
2022-11-30 23:29:28 +0800

    11.30

---
+++ JavaExercise/src/com/wcf11_29/text.java
@@ -0,0 +1,17 @@
+package com.wcf11_29;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class text {
+    public static void main(String[] args) {
+//        ArrayList<student> stu=new ArrayList<>();
+//        stu.add(new student("zhangsan",20));
+//        stu.add(new student("lisi",22));
+//        stu.add(new student("wangwu",23));
+//        String[] a = stu.stream().map(student::lengthen).toArray(String[]::new);
+//        System.out.println(Arrays.toString(a));
+        student []arr=new student[3];
+        System.out.println(arr[0].getAge());
+    }
+}
